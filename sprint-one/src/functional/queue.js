e9b75b64dc2e2a1150b4042c8fda28fd4e6de62c
suprbh <supb27@yahoo.com>
2014-09-10 14:42:43 -0700

    Functional

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,35 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
+
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[size] = value;
+    size++;
+
+    // 
   };
 
   someInstance.dequeue = function(){
+
+    //shift array
+    size && size--;
+    var result;
+    for (var key in storage) {
+      result = storage[key];
+      delete storage[key];
+      break;
+    }
+    //result = storage[0];
+    // delete storage[0];
+    return result;
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,30 @@
 var makeStack = function(){
   var someInstance = {};
-
+ 
   // Use an object with numeric keys to store values
   var storage = {};
+   var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[size] = value;
+    size++;
   };
 
   someInstance.pop = function(){
+    size && size--;
+    var result = storage[size];
+    
+    delete storage[size];
+    
+    return result;
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
 };
+
+// 
