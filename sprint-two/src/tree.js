ed2d76e66b88958a19ccd15475562f98532e65dd
suprbh <supb27@yahoo.com>
2014-09-13 10:11:39 -0700

    add Parent, removeFromParent to tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,11 +1,12 @@
 var makeTree = function(value){
   var newTree = {};
-  
+
   newTree.value = value;
   newTree.children = [];
-  
+  newTree.parent = null;
+
   _.extend(newTree, treeMethods);
-  
+
   return newTree;
 };
 
@@ -15,10 +16,11 @@ treeMethods.addChild = function(value){
 	var child = makeTree(value);
 	var len = this.children.length;
 	this.children[len] = child;
+  child.parent = this;
 };
 
 treeMethods.contains = function(target){
-	
+
 	var hasValue = function (currentNode, value){
 		if (currentNode.value === target) {
 			return true;
@@ -35,6 +37,15 @@ treeMethods.contains = function(target){
 
 	return hasValue(this, target);
 };
+
+treeMethods.removeFromParent = function(){
+  for (var i = 0; i < this.parent.children.length; i++){
+    if(this.parent.children[i] === this){
+      this.parent.children.splice(i);
+    }
+  }
+  this.parent = null;
+}
 
 
 /*
